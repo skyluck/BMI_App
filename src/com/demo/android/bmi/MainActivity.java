2300ca9af2be323b0b7cad0acbb643406ea813c7
skyluck <wasi_3go@hotmail.com>
2015-01-28 20:25:26 +0800

    initial BMI project

---
+++ src/com/demo/android/bmi/MainActivity.java
@@ -0,0 +1,127 @@
+package com.demo.android.bmi;
+
+import java.text.DecimalFormat;
+
+import android.support.v7.app.ActionBarActivity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        initViews();
+        setListensers();
+    }
+    
+    private Button button_calc;
+    private EditText num_height;
+    private EditText num_weight;
+    private TextView show_result;
+    private TextView show_suggest;
+    
+    private void initViews() {
+    	button_calc = (Button) findViewById(R.id.submit);
+    	num_height = (EditText) findViewById(R.id.height);
+    	num_weight = (EditText) findViewById(R.id.weight);
+    	show_result = (TextView) findViewById(R.id.result);
+    	show_suggest = (TextView) findViewById(R.id.suggest);
+    }
+    
+    //Listen for button clicks
+    private void setListensers() {
+    	button_calc.setOnClickListener(calcBMI);
+    }
+    
+    private Button.OnClickListener calcBMI = new OnClickListener () {
+    	public void onClick(View v) {
+    		DecimalFormat nf = new DecimalFormat("0.00");
+    		try {
+    			double height = Double.parseDouble(num_height.getText().toString())/100;
+        		double weight = Double.parseDouble(num_weight.getText().toString());
+        		double BMI = weight / (height*height);
+        		
+        		//Present result
+        		show_result.setText(getText(R.string.bmi_result) + nf.format(BMI));
+        		
+        		//Give health advice
+        		if (BMI > 25) {
+        			show_suggest.setText(R.string.advice_heavy);
+        		} else if (BMI < 20) {
+        			show_suggest.setText(R.string.advice_light);
+        		} else {
+        			show_suggest.setText(R.string.advice_average);
+        		}
+    			
+    		} catch (Exception obj) {
+    			Toast popup = Toast.makeText(MainActivity.this, R.string.input_error, Toast.LENGTH_SHORT);
+    	    	popup.show();
+    		}
+    	}
+    };
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        switch (id) {
+        case R.id.action_about:
+        	openOptionsDialog();
+        	break;
+        case R.id.action_close:
+        	finish();
+        	break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    
+    public void openOptionsDialog() {
+    	AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+    	dialog.setTitle("關於Android BMI");
+    	dialog.setMessage("Android BMI Calc");
+    	dialog.setPositiveButton(android.R.string.ok, 
+    			new DialogInterface.OnClickListener() {
+					
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						// TODO Auto-generated method stub
+						
+					}
+				});
+    	dialog.setNegativeButton(R.string.homepage_label, new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				//open browser
+				Uri uri = Uri.parse(getString(R.string.homepage_uir));
+				Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+				startActivity(intent);
+			}
+		});
+    	dialog.show();
+    }
+}
