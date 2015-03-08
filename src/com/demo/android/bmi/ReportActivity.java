f7c09006c04e0fbe4737b0ec254c7113959a4fbf
skyluck <wasi_3go@hotmail.com>
2015-03-08 19:47:03 +0800

    chapter 18,19

--- src/com/demo/android/bmi/ReportActivity.java
+++ src/com/demo/android/bmi/ReportActivity.java
@@ -3,8 +3,12 @@ package com.demo.android.bmi;
 import java.text.DecimalFormat;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -23,8 +27,10 @@ public class ReportActivity extends Activity {
 	private TextView show_result;
 	private TextView show_suggest;
 	private double BMI;
+	private static final String TAG = "Main";
 	
 	private void initViews () {
+		Log.d(TAG, "init Views");
 		button_back = (Button) findViewById(R.id.report_back);
 		show_result = (TextView) findViewById(R.id.result);
 		show_suggest = (TextView) findViewById(R.id.suggest);
@@ -42,6 +48,7 @@ public class ReportActivity extends Activity {
 		//Give health advice
 		if (BMI > 25) {
 			show_suggest.setText(R.string.advice_heavy);
+			showNotification(BMI);
 		} else if (BMI < 20) {
 			show_suggest.setText(R.string.advice_light);
 		} else {
@@ -51,6 +58,7 @@ public class ReportActivity extends Activity {
 	
 	//Listen for button clicks
 	private void setListensers () {
+		Log.d(TAG, "set Listensers");
 		button_back.setOnClickListener(backtoMain);
 	}
 	
@@ -67,4 +75,28 @@ public class ReportActivity extends Activity {
 			ReportActivity.this.finish();
 		}
 	};
+	
+	protected void showNotification (double BMI) {
+		NotificationManager barManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+		
+//		Notification barMsg = new Notification(
+//			R.drawable.ic_launcher,
+//			"歐歐，你過重囉!",
+//			System.currentTimeMillis()
+//		);
+		
+		PendingIntent contentIntent = PendingIntent.getActivity(this, 0, new Intent(this , MainActivity.class),PendingIntent.FLAG_UPDATE_CURRENT);
+		
+//		barMsg.setLatestEventInfo(ReportActivity.this, "您的BMI值過高", "通知監督人", contentIntent);
+		
+		Notification.Builder barMsg = new Notification.Builder(this)
+			.setTicker("歐歐，你過重囉!")
+			.setContentTitle("您的BMI值過高")
+			.setContentText("通知監督人")
+			.setSmallIcon(android.R.drawable.stat_sys_warning)
+			.setContentIntent(contentIntent);
+		
+		
+		barManager.notify(0,barMsg.build());
+	}
 }
